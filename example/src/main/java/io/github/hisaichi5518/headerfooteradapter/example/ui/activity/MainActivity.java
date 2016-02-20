274ccaeb3c34d7aabddd596d42cd7cdde64cf0c0
hisaichi5518 <hisaichi5518@gmail.com>
2016-02-21 00:22:49 +0900

    Moved MainActivity to ui.activity.Activity

--- example/src/main/java/io/github/hisaichi5518/headerfooteradapter/example/ui/activity/MainActivity.java
+++ example/src/main/java/io/github/hisaichi5518/headerfooteradapter/example/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package io.github.hisaichi5518.headerfooteradapter.example;
+package io.github.hisaichi5518.headerfooteradapter.example.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import io.github.hisaichi5518.headerfooteradapter.example.R;
 
 public class MainActivity extends AppCompatActivity {
 
